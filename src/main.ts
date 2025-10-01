a1ead4360f1c33be2400f9da985588dee6b1dd33
Noololly <od@stephan.co.uk>
2025-10-01 18:39:08 +0100

    Add new font, and HC logo <3

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import './style.css'
+import icon from "/icon-rounded.svg";
 
 function createMainPage() {
     document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <div class="heading">
+      <div>
+        <img class="logo" src="${icon}" alt="Hack Club logo"/>
+      </div>
         <h1>Welcome to Rally!!</h1>
         <div>
           <p>A Hack Club community project where everyone adds their own touch to the website</p>
